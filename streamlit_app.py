6132356e4b467cb6fa068c07a3c6904580b4beae
vmaikweki <vmaikweki@gmail.com>
2025-07-02 17:35:53 -0500

    Update streamlit_app.py
    connected changes needed because of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-import requests
+#import requests
 from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
