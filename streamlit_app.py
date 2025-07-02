80ec6c45bbfcded4846d3ad7ee6221150fc4104f
vmaikweki <vmaikweki@gmail.com>
2025-07-02 18:19:54 -0500

    Update streamlit_app.py
    added an api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 from snowflake.snowpark import session
 
@@ -26,9 +27,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        #st.subheader(fruit_chosen + ' Nutrition Information')
-        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     #st.write(ingredients_string)
 
